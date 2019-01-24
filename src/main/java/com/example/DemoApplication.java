932bf625b7a4114eaaba7f931d5434d747dfe056
Fide Omar <fideislas_prog156@outlook.com>
2019-01-24 10:02:47 -0600

    repositorio - datos de primavera

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -2,7 +2,11 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableJpaRepositories("com.example.repository")
+@EntityScan("org.baeldung.persistence.model")
 @SpringBootApplication
 public class DemoApplication {
 
